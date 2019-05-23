3d43110cef78a752fae8e6c0d76285caf99054da
dujianwu123 <dujianwu123@126.com>
2019-05-23 16:16:46 +0800

    个人简历初始化

---
+++ src/components/resume/Resume.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="mainBox">
+    <section class="loadingBox">
+      <div class="title">
+        <h1>杜建武</h1>
+        <h2>个人H5简历</h2>
+      </div>
+      <div class="progress">
+        <div class="cur"></div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  name: 'Resume'
+}
+</script>
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/Home'
 import Magnifier from '@/components/magnifier/Magnifier'
+import Resume from '@/components/resume/Resume'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/magnifier',
       name: 'Magnifier',
       component: Magnifier
+    },
+    {
+      path: '/resume',
+      name: 'Resume',
+      component: Resume
     }
   ]
 })
